6b6cf095365c959ae332eaf370d964fa8e62a129
shibu@autobse.com <shibu@autobse.com>
2024-09-21 10:48:08 +0530

    updated role based permission

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -9,13 +9,13 @@ import { PaymentWhereUniqueInput } from './dto/unique-payment.input';
 export class PaymentService {
   constructor(private readonly prisma:PrismaService){}
 
-  async createPayment(createPaymentInput: CreatePaymentInput,id:string):Promise<Payment|null> {
+  async createPayment(createPaymentInput: CreatePaymentInput,paymentUserId:string):Promise<Payment|null> {
     try{
       
       return await this.prisma.payment.create({
         data:{...createPaymentInput,
-          createdById:id,
-          userId:id,
+          createdById:paymentUserId,
+          userId:paymentUserId,
   
         }
       })
