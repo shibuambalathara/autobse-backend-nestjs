00772d7fd542503bb402e15a5121422b4e71a6ba
shibuambalathara <shibu@autobse.com>
2024-09-24 09:43:12 +0530

    user updation

--- prisma/migrations/20240924034826_added_id_proof_type/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "UserIdProofTypeType" AS ENUM ('aadhar', 'drivingLicense', 'passport');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "idProofType" "UserIdProofTypeType";
