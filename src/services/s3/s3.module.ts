3fc749e5cb8968372982b182e478e20628320dab
libinautobse <libin@autobse.com>
2024-09-25 14:09:54 +0530

    feat: s3 init

---
+++ src/services/s3/s3.module.ts
@@ -0,0 +1,9 @@
+// import { Module } from '@nestjs/common';
+// import { s3Service } from './s3.service';
+// import { ConfigModule } from '@nestjs/config';
+
+// @Module({
+//     // imports: [ConfigModule],
+//     providers: [s3Service],
+// })
+// export class s3Module { }
---
+++ src/services/s3/s3.service.ts
@@ -0,0 +1,31 @@
+import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class s3Service{
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly s3: S3
+    ) {
+        this.s3 = new S3({
+            region: this.configService.get<string>('AWS_REGION'),
+            credentials: {
+                accessKeyId: this.configService.get<string>('AWS_IAM_ACCESS_KEY'),
+                secretAccessKey: this.configService.get<string>('AWS_IAM_SECRET_ACCESS_KEY'),
+            },
+            
+        })
+    }
+
+    async uploadFile(file: Express.Multer.File, key: string) {
+        const params = {
+            Bucket: process.env.AWS_BUCKET,
+            Key: key,
+            Body: file.buffer,
+            ContentType: file.mimetype
+        }
+        const command = new PutObjectCommand(params)
+        return this.s3.send(command)
+    }
+}
