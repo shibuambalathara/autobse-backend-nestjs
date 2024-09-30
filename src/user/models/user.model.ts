469dd907f029f242cd5022cec11a66611454a4c8
shibuambalathara <shibu@autobse.com>
2024-09-30 09:34:59 +0530

    added payment field on user

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,4 +1,5 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Payment } from 'src/payment/models/payment.model';
 
 @ObjectType()
 export class User {
@@ -61,4 +62,7 @@ export class User {
   @Field()
   idProofType:string;
 
+   @Field(()=>Payment,{nullable:true})
+  payment?:Payment;
+ 
 }
