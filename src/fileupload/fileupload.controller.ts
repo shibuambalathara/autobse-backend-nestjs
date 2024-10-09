4216b10ab972ea3ef28e96493116699059825a85
libinautobse <libin@autobse.com>
2024-10-09 12:41:39 +0530

    feat: vehicle_list_excel_upload

--- src/fileupload/fileupload.controller.ts
+++ src/fileupload/fileupload.controller.ts
@@ -84,25 +84,57 @@ export class FileuploadController {
       }
   }
 
-  @Post('excel')
+  @Post('event_excel')
   // @UseGuards(GqlAuthGuard,RolesGuard)
   // @Roles('admin', 'staff')
   @UseInterceptors(FileInterceptor('file'))
-  async createExcelUpload(
+  async createEventExcelUpload(
     @Param('eventId') eventId: string,
     @UploadedFile() file: Express.Multer.File,
     @Req() req: Request
   ) {
     // const user = req.user
     // console.log(user,'its user');
-    const res = await this.fileuploadService.createExcelUpload('dkfj','djfkd', file, 'name')
+    const res = await this.fileuploadService.createEventExcelUpload('dkfj','djfkd', file, 'name')
     if (!res) throw new InternalServerErrorException('Excel upload failed.')
       return {
         success: true,
         message: 'Excel uploaded sucessfully.',
         res,
       }
-    
+  }
+
+  @Put('vehicle_list_excel/:eventId')
+  // @UseGuards(GqlAuthGuard,RolesGuard)
+  // @Roles('admin', 'staff')
+  @UseInterceptors(FileInterceptor('file',{
+    limits: {
+      fileSize: 2 * 1024 * 1014,
+      files: 1,
+    },
+    fileFilter(req, file, callback) {
+      const allowedExtensions = [ 
+        'application/vnd.ms-excel',
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      ]
+      if (!allowedExtensions.includes(file.mimetype)) {
+        callback(new BadRequestException(`Invalid file type for ${file.fieldname}. Only excel files are allowed.`), false)
+      }
+      callback(null, true)
+    },
+  }))
+  async createVehicleListExcelUpload(
+    @Param('eventId') eventId: string,
+    @UploadedFile() file: Express.Multer.File,
+    // @Req() req: Request
+  ) {
+    const res = await this.fileuploadService.createVehicleListExcelUpload(file, eventId)
+    if (!res) throw new InternalServerErrorException('Excel upload failed.')
+      return {
+        success: true,
+        message: 'Excel uploaded sucessfully.',
+        res,
+      }
   }
   
 }
