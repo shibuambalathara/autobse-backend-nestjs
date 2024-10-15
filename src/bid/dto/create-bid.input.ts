1cb6d34d9e509660f2879733e66bc3fab1c8da3d
shibu@autobse.com <shibu@autobse.com>
2024-10-15 12:10:18 +0530

    updated-bid

--- src/bid/dto/create-bid.input.ts
+++ src/bid/dto/create-bid.input.ts
@@ -2,8 +2,8 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateBidInput {
-  @Field()
-  name:string;
+  // @Field()
+  // name:string;
 
   @Field()
   amount?:number;
