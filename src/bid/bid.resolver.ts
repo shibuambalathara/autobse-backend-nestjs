9850a4a80c390a7fe8145df53436f3ed3d434499
libinautobse <libin@autobse.com>
2024-10-16 14:18:01 +0530

    update: subscription while bid is created.

--- src/bid/bid.resolver.ts
+++ src/bid/bid.resolver.ts
@@ -5,10 +5,14 @@ import { CreateBidInput } from './dto/create-bid.input';
 import { UpdateBidInput } from './dto/update-bid.input';
 import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RedisService } from 'src/services/redis/redis.service';
 
 @Resolver(() => Bid)
 export class BidResolver {
-  constructor(private readonly bidService: BidService) {}
+  constructor(
+    private readonly bidService: BidService,
+    private readonly redisService: RedisService,
+  ) {}
 
   @UseGuards(GqlAuthGuard)
   @Mutation(() => Bid)
@@ -16,7 +20,9 @@ export class BidResolver {
   @Args('bidVehicleId') bidVehicleId:string,
   @Args('createBidInput') createBidInput: CreateBidInput) :Promise<Bid|null>{
     const {id}=context.req.user
-    return this.bidService.createBid(id,bidVehicleId,createBidInput);
+    const bid = await this.bidService.createBid(id,bidVehicleId,createBidInput);
+    this.redisService.publish('BID_CREATION',{bidCreation: bid})
+    return bid
   }
 
   // @Query(() => [Bid])
